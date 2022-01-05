c9fffff9d186cc566386b056493572acad5b313d
Borisa Krstic <borisakrstic02@gmail.com>
2022-01-05 14:13:57 +0100

    Moved getting items and client into ejs2pdf helper

--- api/invoice/invoice.router.js
+++ api/invoice/invoice.router.js
@@ -2,22 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { ejs2PDF } = require("../../utils/ejs-pdf/ejs2PDF");
 
-const graphQlClient = require("../../shared/hasuraClient");
-const { GET_INVOICE_ITEMS } = require("../../graphql/invoice/invoice.queries");
-const { GET_CLIENT } = require("../../graphql/client/client.queries");
-
 router.post("/", async (req, res) => {
   try {
-    const { invoice_items } = await graphQlClient.request(GET_INVOICE_ITEMS, {
-      invoice_id: req.body.invoice_id,
-    });
-    console.log("invoice_items: ", invoice_items);
-
-    const { clients_by_pk } = await graphQlClient.request(GET_CLIENT, {
-      id: req.body.client_id,
-    });
-    console.log("clients_by_pk: ", clients_by_pk);
-
     const fileName = "invoice-" + JSON.stringify(Date.now()) + ".pdf";
     await ejs2PDF(fileName, req.body);
     res.send(fileName);
--- utils/ejs-pdf/ejs2PDF.js
+++ utils/ejs-pdf/ejs2PDF.js
@@ -3,10 +3,24 @@ const pdf = require("html-pdf");
 const ejs = require("ejs");
 const { emailClient } = require("../../shared/email");
 
-const invoiceCalculate = (ejsData) => {
-  // Transform ejsData to pdfData
+const graphQlClient = require("../../shared/hasuraClient");
+const { GET_INVOICE_ITEMS } = require("../../graphql/invoice/invoice.queries");
+const { GET_CLIENT } = require("../../graphql/client/client.queries");
+
+const invoiceCalculate = async (requestData) => {
+  // Get invoice items
+  const { invoice_items } = await graphQlClient.request(GET_INVOICE_ITEMS, {
+    invoice_id: requestData.invoice_id,
+  });
+
+  // Get client
+  const { clients_by_pk } = await graphQlClient.request(GET_CLIENT, {
+    id: requestData.client_id,
+  });
+
+  // Transform requestData to pdfData
   // Calculate total for each item
-  const items = ejsData.items.map((item, index) => {
+  const items = requestData.items.map((item, index) => {
     return {
       index: index + 1,
       name: item.name,
@@ -25,14 +39,14 @@ const invoiceCalculate = (ejsData) => {
 
   // Calculate discount based on percentage value
   const discount = parseFloat(
-    (subtotal * ejsData.discount_in_percent) / 100
+    (subtotal * requestData.discount_in_percent) / 100
   ).toFixed(2);
 
   // Calculate total with discount
   const total = parseFloat(subtotal - discount).toFixed(2);
 
   const pdfData = {
-    ...ejsData,
+    ...requestData,
     items,
     subtotal,
     discount,
@@ -42,7 +56,8 @@ const invoiceCalculate = (ejsData) => {
 };
 
 // Generate html invoice
-const ejs2PDF = (fileName, ejsData) => {
+const ejs2PDF = async (fileName, requestData) => {
+  const pdfData = await invoiceCalculate(requestData);
   // Read ejs file
   fs.readFile(__dirname + "/invoice.ejs", "utf8", async (err, data) => {
     if (err) {
@@ -52,9 +67,9 @@ const ejs2PDF = (fileName, ejsData) => {
     // Render ejs file
     const ejs_string = data;
     const template = ejs.compile(ejs_string);
-    const html = template(invoiceCalculate(ejsData));
+    const html = template(pdfData);
     // Generate pdf
-    await generatePdf(fileName, ejsData.email, html);
+    await generatePdf(fileName, requestData.email, html);
   });
 };
 
