704d3fbe7ba767d124ebbc362995c770326eead2
Borisa Krstic <borisakrstic02@gmail.com>
2022-01-05 09:23:15 +0100

    Improving code quality

---
+++ api/invoice/invoice.router.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const { ejs2PDF } = require("../../utils/ejs-pdf/ejs2PDF");
+
+router.post("/", async (req, res) => {
+  try {
+    const fileName = "invoice-" + JSON.stringify(Date.now()) + ".pdf";
+    await ejs2PDF(fileName, req.body);
+    res.send(fileName);
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const { ejs2PDF } = require("./utils/ejs-pdf/ejs2PDF");
+const useRouter = require("./api/index");
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(__dirname + "/uploads"));
-
-app.post("/api/html-to-pdf", async (req, res) => {
-  try {
-    const fileName = "invoice-" + JSON.stringify(Date.now()) + ".pdf";
-    await ejs2PDF(fileName, req.body);
-    res.send(fileName);
-  } catch (error) {
-    res.status(500).send("Internal Server Error");
-  }
-});
+useRouter(app);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () =>
