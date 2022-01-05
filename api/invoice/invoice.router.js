33692142691b95e05df6121573836d0f0fe8267c
Borisa Krstic <borisakrstic02@gmail.com>
2022-01-05 13:53:34 +0100

    Added query for invoice_client

--- api/invoice/invoice.router.js
+++ api/invoice/invoice.router.js
@@ -4,6 +4,7 @@ const { ejs2PDF } = require("../../utils/ejs-pdf/ejs2PDF");
 
 const graphQlClient = require("../../shared/hasuraClient");
 const { GET_INVOICE_ITEMS } = require("../../graphql/invoice/invoice.queries");
+const { GET_CLIENT } = require("../../graphql/client/client.queries");
 
 router.post("/", async (req, res) => {
   try {
@@ -11,6 +12,12 @@ router.post("/", async (req, res) => {
       invoice_id: req.body.invoice_id,
     });
     console.log("invoice_items: ", invoice_items);
+
+    const { clients_by_pk } = await graphQlClient.request(GET_CLIENT, {
+      id: req.body.client_id,
+    });
+    console.log("clients_by_pk: ", clients_by_pk);
+
     const fileName = "invoice-" + JSON.stringify(Date.now()) + ".pdf";
     await ejs2PDF(fileName, req.body);
     res.send(fileName);
