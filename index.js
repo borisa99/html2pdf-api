383a9a20b5ff0213af21edfb921532459c6e6169
borisa99 <borisakrstic02@gmail.com>
2022-01-05 07:02:00 +0100

    Multer cleanup

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const express = require("express");
 const app = express();
-const { create } = require("./utils/htmltopdf");
-const { upload } = require("./multer");
+// const { create } = require("./utils/htmltopdf");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + "/uploads"));
 
-app.post("/api/html-to-pdf", upload, async (req, res) => {
+app.post("/api/html-to-pdf", async (req, res) => {
   try {
-    const pdfFileName = `${req.fileName.split(".")[0]}.pdf`;
-
-    await create(req.fileName, pdfFileName);
-    res.send(pdfFileName);
+    // await create(req.fileName, pdfFileName);
+    res.send("DOOBAR");
   } catch (error) {
     res.status(500).send("Internal Server Error");
   }
