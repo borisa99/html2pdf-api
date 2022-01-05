b09de2b6209253aabbb497d86e61cdea8f983f16
Borisa Krstic <borisakrstic02@gmail.com>
2022-01-05 09:10:44 +0100

    Added calculating invoice total,grand total...

--- utils/ejs-pdf/ejs2PDF.js
+++ utils/ejs-pdf/ejs2PDF.js
@@ -3,6 +3,44 @@ const pdf = require("html-pdf");
 const ejs = require("ejs");
 const { emailClient } = require("../../shared/email");
 
+const invoiceCalculate = (ejsData) => {
+  // Transform ejsData to pdfData
+  // Calculate total for each item
+  const items = ejsData.items.map((item, index) => {
+    return {
+      index: index + 1,
+      name: item.name,
+      description: item.description,
+      quantity: item.quantity,
+      price: item.price,
+      total: parseFloat(item.price * item.quantity).toFixed(2),
+    };
+  });
+  // Calculate subtotal
+  const subtotal = parseFloat(
+    items.reduce((acc, item) => {
+      return acc + item.price * item.quantity;
+    }, 0)
+  ).toFixed(2);
+
+  // Calculate discount based on percentage value
+  const discount = parseFloat(
+    (subtotal * ejsData.discount_in_percent) / 100
+  ).toFixed(2);
+
+  // Calculate total with discount
+  const total = parseFloat(subtotal - discount).toFixed(2);
+
+  const pdfData = {
+    ...ejsData,
+    items,
+    subtotal,
+    discount,
+    total,
+  };
+  return pdfData;
+};
+
 // Generate html invoice
 const ejs2PDF = (fileName, ejsData) => {
   // Read ejs file
@@ -14,7 +52,7 @@ const ejs2PDF = (fileName, ejsData) => {
     // Render ejs file
     const ejs_string = data;
     const template = ejs.compile(ejs_string);
-    const html = template(ejsData);
+    const html = template(invoiceCalculate(ejsData));
     // Generate pdf
     await generatePdf(fileName, html);
   });
